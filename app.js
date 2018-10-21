789165e72a4e9bec1a1b76e24eaa63a64b9a0bd7
thomasthuyaaung <thu.ya.a@gmail.com>
2018-10-21 18:18:03 -0400

    https:

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var bodyParser 	= require('body-parser');
-
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
 
 var app = express();
 var router = express.Router({mergeParams: true});
+
+var credentials = {
+  key: fs.readFileSync('client-key.pem'),
+  cert: fs.readFileSync('client-cert.pem')
+};
 
 //this will allow serving files in /bower_components at /bower_components
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
@@ -81,11 +88,34 @@ app.get('/*', function(req, res){
 
 // Server config
 
-var server = app.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+// var server = app.listen(3000, function () {
+//     var host = server.address().address;
+//     var port = server.address().port;
 
-    console.log('Example app listening at http://localhost:3000');
-});
+//     console.log('Example app listening at http://localhost:3000');
+// });
+
+
+// Create an HTTP service.
+// var server = http.createServer(app).listen(3000);
+// Create an HTTPS service identical to the HTTP service.
+// https.createServer(options, app).listen(443);
+
+
+// var httpServer = http.createServer(app);
+// var httpsServer = https.createServer(credentials, app);
+
+// httpServer.listen(8080);
+// httpsServer.listen(8443);
+
+
+var server = https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app)
+.listen(3000, function () {
+  console.log('Example app listening on port 3000! Go to https://localhost:3000/')
+})
+
 
 module.exports = server
